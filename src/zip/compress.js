1b01ec764a419551491acd8f6fb2ae65b359cdee
Vadim <javadimus@gmail.com>
2025-04-28 22:43:55 +0300

    feat: add zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = join('src', 'zip', 'files', 'fileToCompress.txt');
+    const archivePath = join('src', 'zip', 'files', 'archive.gz');
+
+    const readStream = createReadStream(filePath);
+    const writeStream = createWriteStream(archivePath);
+    const gzip = createGzip();
+
+    readStream.pipe(gzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('File compressed successfully!');
+    });
+
+    writeStream.on('error', (err) => {
+        console.error('Error:', err.message);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const archivePath = join('src', 'zip', 'files', 'archive.gz');
+    const outputPath = join('src', 'zip', 'files', 'fileToCompress.txt');
+
+    const readStream = createReadStream(archivePath);
+    const writeStream = createWriteStream(outputPath);
+    const gunzip = createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('File decompressed successfully!');
+    });
+
+    writeStream.on('error', (err) => {
+        console.error('Error:', err.message);
+    });
+
 };
 
 await decompress();

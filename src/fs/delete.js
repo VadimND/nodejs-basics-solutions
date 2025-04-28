5304b474d172bbea5f7a3891f7265e00438249d3
Vadim <javadimus@gmail.com>
2025-04-28 17:46:00 +0300

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    
+    const filePath = join('src', 'fs', 'files', 'fileToRemove.txt');
+
+    try {
+        
+        await fs.access(filePath);
+       
+        await fs.unlink(filePath);
+
+        console.log('File deleted successfully!');
+
+    } catch (err) {
+
+        throw new Error('FS operation failed');
+
+    }
 };
 
 await remove();

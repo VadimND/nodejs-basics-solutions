88c4f3da55e7972b85aadac7550e569a06792357
Vadim <javadimus@gmail.com>
2025-04-28 17:22:19 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,38 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const copy = async () => {
-    // Write your code here 
+
+    const srcDir = join('src', 'fs', 'files');
+    const destDir = join('src', 'fs', 'files_copy');
+
+    try {
+
+        await fs.access(srcDir);
+
+        try {
+
+            await fs.access(destDir);
+
+            throw new Error('FS operation failed');
+
+        } catch (err) {
+
+            if (err.code !== 'ENOENT') {
+                throw err;
+            }
+
+        }
+
+        await fs.cp(srcDir, destDir, { recursive: true });
+
+        console.log('Folder copied successfully!');
+
+    } catch (err) {
+
+        throw new Error('FS operation failed');
+        
+    }
 };
 
 await copy();

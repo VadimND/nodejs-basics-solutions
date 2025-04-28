e803846299beff17ece39191b1e61b827b101cd0
Vadim <javadimus@gmail.com>
2025-04-28 18:48:07 +0300

    feat: add CLI functions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,20 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssEnv = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    if (rssEnv) {
+        console.log(rssEnv);
+    } else {
+        console.log('No RSS_ environment variables found.');
+    }
 };
+
+// Set test variables
+process.env.RSS_NAME = "test";
+process.env.RSS_PORT = "1234";
+
+// Check: node src/cli/env.js
 
 parseEnv();

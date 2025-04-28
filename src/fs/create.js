635d26364b151c80e0294b521d23ab39db514ee1
Vadim <javadimus@gmail.com>
2025-04-28 13:41:12 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { promises as fs } from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = 'src/fs/files/fresh.txt';
+    const text = 'I am fresh and young';
+
+    try {
+        await fs.writeFile(filePath, text, { flag: 'wx' });
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await create();

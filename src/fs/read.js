0224c5c10adb93abac8792b5acfddac224b651ee
Vadim <javadimus@gmail.com>
2025-04-28 18:06:00 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join('src', 'fs', 'files', 'fileToRead.txt');
+
+    try {
+        await fs.access(filePath);
+
+        const info = await fs.readFile(filePath, 'utf8');
+
+        console.log(info);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();

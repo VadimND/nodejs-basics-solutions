89adff2bfc81fcc94324590d0c239b963d096478
Vadim <javadimus@gmail.com>
2025-04-28 22:02:10 +0300

    feat: add hash calculate

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+    stream.on('data', (chunk) => hash.update(chunk));
+    stream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
 };
 
 await calculateHash();

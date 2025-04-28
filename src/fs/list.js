4fe6adc0f3e84c4f585b2ced3a2f220278fde2b5
Vadim <javadimus@gmail.com>
2025-04-28 18:03:00 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = join('src', 'fs', 'files');
+
+    try {
+        
+        await fs.access(dirPath);
+
+        const files = await fs.readdir(dirPath);
+
+        console.log(files);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();

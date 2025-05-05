4ced42bda8f7bd68852dacdb42b17204c6f71ce3
Vadim <javadimus@gmail.com>
2025-05-05 14:56:37 +0300

    Fix: return .cjs file and add import

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,26 +1,27 @@
-import path from 'path';
-import { release, version } from 'os';
-import { createServer as createServerHttp } from 'http';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { readFile } from 'fs/promises';
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import { readFile } from "fs/promises";
+import("./files/c.cjs");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const random = Math.random();
 
-let unknownObject;
+let unknownObject = {};
 
-const filePath = random > 0.5
-  ? path.join(__dirname, './files/a.json')
-  : path.join(__dirname, './files/b.json');
+const filePath =
+  random > 0.5
+    ? path.join(__dirname, "./files/a.json")
+    : path.join(__dirname, "./files/b.json");
 
 try {
-  const fileData = await readFile(filePath, 'utf-8');
+  const fileData = await readFile(filePath, "utf-8");
   unknownObject = JSON.parse(fileData);
 } catch (err) {
-  console.error('Failed to load JSON:', err);
+  console.error("Failed to load JSON:", err);
 }
 
 console.log(`Release ${release()}`);
@@ -31,7 +32,7 @@ console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
 const myServer = createServerHttp((_, res) => {
-  res.end('Request accepted');
+  res.end("Request accepted");
 });
 
 const PORT = 3000;
@@ -40,7 +41,7 @@ console.log(unknownObject);
 
 myServer.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-  console.log('To terminate it, use Ctrl+C combination');
+  console.log("To terminate it, use Ctrl+C combination");
 });
 
 export { unknownObject, myServer };
